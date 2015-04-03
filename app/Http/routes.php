35b8b3fbed91f7d9489a83af7cf3dc2ad1d9d0b4
Sam <samcharmy@gmail.com>
2015-04-03 22:43:37 +0200

    Add user authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,24 +1,27 @@
 <?php
 
-/*
- * Home
- */
-Route::get('/', function () {
-    return view('master');
-});
-/*
- * Articles
- */
-Route::resource('notes', 'NotesController');
-Route::resource('categories', 'CategoriesController');
-Route::resource('platforms', 'PlatformsController');
-Route::resource('languages', 'LanguagesController');
-Route::resource('frameworks', 'FrameworksController');
-Route::resource('tags', 'TagsController');
-
-Route::post('search/{search}', ['as' => 'search', 'uses' => 'NotesController@search']);
-
 Route::controllers([
     'auth'     => 'Auth\AuthController',
     'password' => 'Auth\PasswordController',
 ]);
+
+Route::group(['middleware' => 'auth'], function () {
+
+    /*
+     * Home
+     */
+    Route::get('/', function () {
+        return view('master');
+    });
+
+    Route::resource('notes', 'NotesController');
+    Route::resource('categories', 'CategoriesController');
+    Route::resource('platforms', 'PlatformsController');
+    Route::resource('languages', 'LanguagesController');
+    Route::resource('frameworks', 'FrameworksController');
+    Route::resource('tags', 'TagsController');
+
+    Route::post('search/{search}', ['as' => 'search', 'uses' => 'NotesController@search']);
+
+});
+
