0390b09ad2bd97d4db7d22e26689119703108a9e
Sam <samcharmy@gmail.com>
2015-04-04 13:41:07 +0200

    Add attribute data to note.show template.

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -7,14 +7,59 @@
 @section('content')
 
 <h1>{{ $note->title }}</h1>
-
+<br/>
 <article>{!! $note->body !!}</article>
 
-<br/>
-<div>
-<a href="{{ route('notes.index') }}" class="btn btn-default">Back</a>
-<a href="{{ route('notes.edit', $note->id) }}" class="btn btn-primary">Edit</a>
+<hr/>
+<div class="row note-attributes">
+<h3>Metadeta</h3>
+    <div class="col-md-2">
+        <h5>Type</h5>
+        <span>{{$note->type}}</span>
+    </div>
+    <div class="col-md-2">
+        <h5>Category</h5>
+        <span>{{$note->category->name}}</span>
+    </div>
+    <div class="col-md-2">
+        <h5>Platforms</h5>
+        <ul>
+            @foreach($note->platforms as $platform)
+                <li>{{ $platform->name }}</li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-md-2">
+        <h5>Languages</h5>
+        <ul>
+            @foreach($note->languages as $language)
+                <li>{{ $language->name }}</li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-md-2">
+        <h5>Frameworks</h5>
+        <ul>
+            @foreach($note->frameworks as $framework)
+                <li>{{ $framework->name }}</li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="col-md-2">
+        <h5>Tags</h5>
+        <ul>
+            @foreach($note->tags as $tag)
+                <li>{{ $tag->name }}</li>
+            @endforeach
+        </ul>
+    </div>
 </div>
+<hr/>
+<div class="row">
+    <a href="{{ route('notes.index') }}" class="btn btn-default">Back</a>
+    <a href="{{ route('notes.edit', $note->id) }}" class="btn btn-primary">Edit</a>
+</div>
+
 
 @stop
 
